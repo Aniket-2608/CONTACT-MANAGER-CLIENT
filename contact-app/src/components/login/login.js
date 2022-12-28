2f19e02f4f06171d61568c3f40a7155cbd3f7611
AnkitChaudhary67 <ankit67.ac@gmail.com>
2022-12-28 17:01:38 +0530

    modified login and sign up page

--- contact-app/src/components/login/login.js
+++ contact-app/src/components/login/login.js
@@ -1,30 +1,47 @@
 import './login.css'
+import { useState } from 'react'
 const Login=()=>{
+  const [credentials, setCredentials]=useState({email:"" ,password:""});
+  const handleChange=(event)=>{
+    const {name,value}=event.target;
+    setCredentials({...credentials,[name]:value});
+  };
+  
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+  };
+
     return(
          <>
          <section className='main'>
             
-            <form>
+            <form onSubmit={handleSubmit}>
             <section className='logo'>
             <img src={require("../images/logo.png")} alt="log"/>
             </section>
             <p>
-            Enter your credentials to access your account
-                </p>
-            
-            <section className="email">
-              <input type="email"  placeholder="Email"/>
-            </section>
-            <section className='password'>
-              <input type="password" placeholder="Password" />
-            </section>
-            
-            <button>
-              Sign In
-            </button>
-            <button>
+            Enter your credentials to acc<button>
               Sign Up
             </button>
+            <section className="email">
+              <input type="email"  placeholder="Email" name='email' value={credentials.email} onChange={handleChange} required/>
+            </section>
+            <section className='password'>
+              <input type="password" placeholder="Password"  name='password' value={credentials.password} onChange={handleChange} required/>
+            </section>
+            <section>
+                <input type="submit" value="Sign In" />
+            </section>
+            <section>
+              <Link to="/signup">
+              <button>
+              Sign Up
+            </button>
+              </Link>
+            
+            </section>
+            
+           
             </form>
             
 
--- contact-app/src/components/signup/signup.js
+++ contact-app/src/components/signup/signup.js
@@ -1,29 +1,37 @@
 
 const SignUp=()=>{
+  const [credentials, setCredentials]=useState({email:"" ,password:"",confirmpassword:""});
+  const handleChange=(event)=>{
+    const {name,value}=event.target;
+    setCredentials({...credentials,[name]:value});
+  };
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+  };
     return(
          <>
          <section className='main'>
             <section className='logo'>
             <img src={require("../images/logo.png")} alt="log"/>
             </section>
-            <form>
+            <form onSubmit={handleSubmit}> 
             
             <p>
             Create New Account
                 </p>
             
             <section className="email">
-              <input type="email" placeholder="Email" />
+              <input type="email" placeholder="Email" name='email' value={credentials.email} required  />
             </section>
             <section className='password'>
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" name='password' value={credentials.password} required  />
             </section>
             <section className='password'>
-              <input type="password" placeholder="Confirm  Password"/>
+              <input type="password" placeholder="Confirm  Password" name='confirmpassword' value={credentials.confirmpassword} required/>
             </section>
-            <button>
-              Sign Up
-            </button>
+            <section>
+                <input type="submit" value="Sign Up" />
+            </section>
 
             </form>
             
