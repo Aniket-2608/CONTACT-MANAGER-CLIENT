8155a3521021ca7b30abf256f72d2b455d9db1a4
AnkitChaudhary67 <ankit67.ac@gmail.com>
2023-01-03 10:14:51 +0530

    modified app file

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -3,19 +3,23 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './components/login/login';
 import SignUp from './components/signup/signup';
-import { AuthContextPro } from './context/authcontext';
+import {AuthContextPro } from './context/authcontext';
+import { ToastContextProvider } from './context/ToastContext';
 
 function App() {
   return (
    <>
+   <ToastContextProvider>
    <AuthContextPro>
    <BrowserRouter>
    <Routes>
-    <Route path='/' element={<Login/>}/>
+    <Route path='/login' element={<Login/>}/>
     <Route path='/sign-up' element={<SignUp/>}/>
    </Routes>
    </BrowserRouter>
    </AuthContextPro>
+   </ToastContextProvider>
+   
   
    </>
   );
